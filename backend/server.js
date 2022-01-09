ec1150d78e559f5656d78c0ff234d23e3c5d185d
Jeremiah Thomas <jtreplies@gmail.com>
2022-01-09 16:27:08 +0400

    added cheerio parsing and call for mint and marketwatch

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,77 @@ app.get("/guardian", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+function cleanTitle(title){
+
+    let startIdx = 0;
+    for(let i=0; i< title.length ; i++){
+
+        if ((/[A-Z]/).test(title[i])) {
+            startIdx = i;
+            console.log(startIdx);
+            break;
+            
+    }
+
+    return title.slice(startIdx);    
+
+}
+}
+
+app.get("/mint", (req, res) => {
+    axios("https://www.livemint.com/")
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const newsItems = [];
+  
+        $("h2.headline").each(function () {
+          const title = $(this).text();
+          console.log(title.replace(/\n/g, ''));
+          const url = $(this).find("a").attr("href");
+          newsItems.push({
+            title,
+            url,
+          });
+        });
+        res.json(newsItems);
+      })
+      .catch((err) => console.log(err));
+  });
+  
+
+app.get("/marketwatch", (req, res) => {
+    axios("https://www.marketwatch.com/")
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const newsItems = [];
+  
+        $("a.latestNews__headline").each(function () {
+          // const title = $(this).text()
+          const baseElement = $(this);
+          const title = baseElement.find(".headline").text();
+          const url = baseElement.attr("href");
+          newsItems.push({ title, url});
+        });
+
+        $("h3.article__headline").each(function () {
+            // const title = $(this).text()
+            const baseElement = $(this);
+            const title = baseElement.text();
+            const url = baseElement.find("a").attr("href");
+            newsItems.push({ title, url});
+          });
+
+
+        res.send(newsItems);
+  
+      })
+      .catch((err) => console.log(err));
+  });
+  
+
+
 app.get("/bbcnews", (req, res) => {
   axios("https://www.bbc.com/news")
     .then((response) => {
